9bf36b28805a88d5d0b13964bc6cc9d3b603b22b
Anton <31567808+Anton-Khan@users.noreply.github.com>
2021-03-19 18:03:34 +0300

    Добавьте файлы проекта.

---
+++ Filtering/JaccardIndex.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Filtering
+{
+    class JaccardIndex
+    {
+        private List<double[]> _table;
+        private List<double[]> _transponsedTable;
+        public JaccardIndex( List<double[]> table)
+        {
+            _table = table;
+            _transponsedTable = TransposeTable();
+        }
+
+        public List<double[]> FindSimilarItems()
+        {
+            return FindSimilar(_transponsedTable);
+        }
+
+        public List<double[]> FindSimilarUsers()
+        {
+
+            return FindSimilar(_table);
+        }
+
+        public List<double[]> FindSimilar(List<double[]> table)
+        {
+            List<double[]> similarities = new List<double[]>();
+            for (int i = 0; i < table.Count-1; i++)
+            {
+                double[] relations = new double[table[0].Length];
+                for (int j = i+1; j < table.Count; j++)
+                {
+                    int sum = 0;
+                    for (int k = 0; k < table[0].Length; k++)
+                    {
+                        if (table[i][k] == table[j][k])
+                        {
+                            sum++;
+                        }
+                    }
+                    relations[j] = sum / (double)table[0].Length;
+                    
+                }
+                similarities.Add(relations);
+            }
+            return similarities;
+        }
+
+        public List<double[]> TransposeTable()
+        {
+            List<double[]> transponsedTable = new List<double[]>();
+
+            for (int i = 0; i < _table[0].Length; i++)
+            {
+                double[] users = new double[_table.Count];
+
+                for (int j = 0; j < _table.Count; j++)
+                {
+                    users[j] = _table[j][i];
+                }
+                transponsedTable.Add(users);
+            }
+            return transponsedTable;
+        }
+
+    }
+}
---
+++ Filtering/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Filtering
+{
+    class Program
+    {
+
+        static List<double[]> _table;
+
+        static void CreateTable(int users, int items, int minGrade, int maxGrade)
+        {
+            _table = new List<double[]>();
+            Random rand = new Random();
+            for (int i = 0; i < users; i++)
+            {
+                double[] i_items = new double[items]; 
+                for (int j = 0; j < items; j++)
+                {
+                    i_items[j] = rand.Next(minGrade, maxGrade+1);
+                }
+
+                _table.Add(i_items);
+            }
+        }
+
+        static void DisplayTable(List<double[]> table, string str, string col)
+        {
+            foreach (var item in table.First())
+            {
+                Console.Write("\t" + col );
+            }
+            Console.WriteLine();
+            foreach (var user in table)
+            {
+                Console.Write(str + "\t");
+                foreach (var grade in user)
+                {
+                    Console.Write(grade + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            CreateTable(10, 10, 0, 1);
+            DisplayTable(_table, "user", "item");
+            Console.WriteLine();
+            JaccardIndex jc = new JaccardIndex(_table);
+            DisplayTable(jc.FindSimilarUsers(), "user", "item");
+            Console.WriteLine();
+            DisplayTable(jc.FindSimilarItems(), "user", "item");
+        }
+    }
+}
